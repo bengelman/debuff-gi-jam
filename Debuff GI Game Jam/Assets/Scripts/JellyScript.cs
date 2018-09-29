02feb55e2326eba23e3b6c2d9978db036161df3b
david <jannaultheal@gmail.com>
2018-09-29 14:20:50 -0400

    Jellyfish knockback

--- Debuff GI Game Jam/Assets/Scripts/JellyScript.cs
+++ Debuff GI Game Jam/Assets/Scripts/JellyScript.cs
@@ -9,10 +9,13 @@ public class JellyScript : MonoBehaviour {
 	Vector2 targetPosition = new Vector2(0,0);
 	float left_offset = 0f;
 	float right_offset = 0f;
+	int time_to_next_change = 0;
+	int time_per_change = 30;
+	System.Random rnd;
 	// Use this for initialization
 	void Start () {
 		Character = GameObject.Find("Character");
-		System.Random rnd = new System.Random(GetInstanceID()  +(int)System.DateTime.Now.TimeOfDay.TotalMilliseconds);
+		rnd = new System.Random(GetInstanceID()  +(int)System.DateTime.Now.TimeOfDay.TotalMilliseconds);
 		left_offset = (float)rnd.NextDouble()*6-3;
 		right_offset = (float)rnd.NextDouble()*6-3;
 		Debug.Log(left_offset);
@@ -31,16 +34,27 @@ public class JellyScript : MonoBehaviour {
 			new Vector2(transform.position.x, transform.position.y)
 			, targetPosition
 			, speed * Time.deltaTime);
+		time_to_next_change -= 1;
+		if(time_to_next_change <= 0){
+			left_offset = (float)rnd.NextDouble()*6-3;
+			right_offset = (float)rnd.NextDouble()*6-3;	
+			time_to_next_change = time_per_change;
+		}
 	}
 	void LateUpdate()
 	{
 		GetComponent<SpriteRenderer>().sortingOrder = -(int)(transform.position.y * 100);
 	}
-	void OnCollisionEnter2D(Collision2D col)
+	void OnCollisionStay2D(Collision2D col)
 	{
 		if (col.gameObject.GetComponent<LivingEntity> ()) {
-			if (col.gameObject.GetComponent<PlayerScript> ()) {
+			if (col.gameObject.GetComponent<PlayerScript> ()) { // if it has a player script
 				col.gameObject.GetComponent<LivingEntity> ().Hurt ();
+				//knocks back the target
+				Debug.Log(col.gameObject.transform.position);
+				Debug.Log(this.transform.position);
+				col.gameObject.transform.position -= (this.transform.position-col.gameObject.transform.position)*3;
+				
 			}
 		}
 	}	
