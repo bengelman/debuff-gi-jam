c5e030bf06e577ed8f779b6507ec84d355bbaaf6
bengelman <brendan.engelman@gmail.com>
2018-09-29 16:07:13 -0400

    Finished main menu

---
+++ Debuff GI Game Jam/Assets/NewGameButton.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class NewGameButton : MonoBehaviour {
+	public Button start;
+	// Use this for initialization
+	void Start () {
+		start.onClick.AddListener (TaskOnClick);
+	}
+	void TaskOnClick(){
+		SceneManager.LoadScene ("Overworld");
+	}
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
