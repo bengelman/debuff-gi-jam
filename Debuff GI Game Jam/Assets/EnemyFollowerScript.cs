8cb67c45ba2abaf4b1c8467ce3f674e5ce4ac9b3
FengWei Pi <fengwei_2013@hotmail.com>
2018-09-29 10:27:18 -0400

    Added Enemy ai

---
+++ Debuff GI Game Jam/Assets/EnemyFollowerScript.cs
@@ -0,0 +1,29 @@
+﻿﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFollowerScript : MonoBehaviour {
+	
+	GameObject Player;
+	public float speed = 2.0f;
+	Vector2 playerPosition = new Vector2(0,0);
+
+	// Use this for initialization
+	void Start () {
+		Player = GameObject.Find("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// find player position
+		playerPosition = new Vector2(
+			Player.transform.position.x
+			, Player.transform.position.y);
+		
+		// move towards player
+		transform.position = Vector2.MoveTowards(
+			new Vector2(transform.position.x, transform.position.y)
+			, playerPosition
+			, speed * Time.deltaTime);
+	}
+}
